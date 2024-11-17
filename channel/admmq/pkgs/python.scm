b62a7bf7b8a7c274da2b986322a1eaeeef6b6fda
Adam <>
2024-11-17 05:52:42 +0300

    wip

--- channel/admmq/pkgs/python.scm
+++ channel/admmq/pkgs/python.scm
@@ -37,3 +37,33 @@
     (description
      "")
     (license #f)))
+
+(define-public python-tcod
+  (package
+    (name "python-tcod")
+    (version (git-version "0.0.1" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/libtcod/python-tcod.git")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1fqzpr7nw4n0wa0wwv2z3nw3xzihfsasn16hhxh93q3dp5padvhd"))))
+    (build-system python-build-system)
+    ;; (arguments
+    ;;  (list #:tests? #f
+    ;;        #:phases
+    ;;        #~(modify-phases %standard-phases
+    ;;          ;; sanity-check phase fail, but the application seems to be working
+    ;;          (delete 'sanity-check))))
+    ;; (inputs
+    ;;  (list zlib
+    ;;        ijg-libjpeg
+    ;;        libheif))
+    (home-page "")
+    (synopsis "")
+    (description
+     "")
+    (license #f)))
