3b9c863ea072a997213c670b7f9dbb4926595378
Adam <>
2024-11-17 15:55:08 +0300

    wip

--- channel/admmq/pkgs/python.scm
+++ channel/admmq/pkgs/python.scm
@@ -9,6 +9,7 @@
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages python-web)
   #:use-module (gnu packages python-build)
+  #:use-module (gnu packages check)
   #:use-module (gnu packages libffi)
   #:use-module (gnu packages c)
   #:use-module (guix packages)
